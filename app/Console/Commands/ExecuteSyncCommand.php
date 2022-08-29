ffd529fde296c878d23e5cc971b5c3d486e88875
mayur hirpara <mayur@cloudcraftsmanship.io>
2022-08-29 11:27:00 +0530

    Refresh expired discord token

--- app/Console/Commands/ExecuteSyncCommand.php
+++ app/Console/Commands/ExecuteSyncCommand.php
@@ -2,6 +2,7 @@
 
 namespace App\Console\Commands;
 
+use App\Jobs\DiscordSyncJob;
 use App\Jobs\SlackSyncJob;
 use App\Models\Account;
 use Illuminate\Console\Command;
@@ -30,10 +31,14 @@ class ExecuteSyncCommand extends Command
     public function handle()
     {
         Account::query()
-            ->where('platform', 'slack')
+//            ->where('platform', 'slack')
             ->chunk(100, function ($accounts) {
                 foreach ($accounts as $account) {
-                    dispatch(new SlackSyncJob($account));
+                    if ($account->platform == 'slack') {
+                        dispatch(new SlackSyncJob($account));
+                    } elseif ($account->platform == 'discord') {
+                        dispatch(new DiscordSyncJob($account));
+                    }
                 }
             });
 
